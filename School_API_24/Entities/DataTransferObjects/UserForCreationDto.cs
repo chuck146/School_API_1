2e9062eebbde9e160391536419de49f5b81108c9
sam98@njit.edu <sam98@njit.edu>
2020-07-16 18:56:06 -0400

    Task: initiated validation on models

--- School_API_24/Entities/DataTransferObjects/UserForCreationDto.cs
+++ School_API_24/Entities/DataTransferObjects/UserForCreationDto.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Entities.DataTransferObjects
 {
     public class UserForCreationDto
     {
+        [Required(ErrorMessage = "User name is a required field.")]
+        [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Age is a required field.")]
         public int Age { get; set; }
+
+        [Required(ErrorMessage = "Position is a required field.")]
+        [MaxLength(20, ErrorMessage = "Maximum length for the position is 20 characters.")]
         public string Position { get; set; }
     }
 }
